01fafbd2dae9315951a9f7943a5991c6dcb5fd45
Miguel(MgARL) <mgarlcr@gmail.com>
2021-11-05 19:22:05 -0400

    Full List Accordeon Working

--- script.js
+++ script.js
@@ -19,13 +19,13 @@ let dishesList = [
     {
         dishName: 'Meatballs with Cauliflower Rice',
         ingredients: 'Ground Beef, Your Choice of Sesoning, Riced Cauliflower, Almond flour, Eggs'
-    },{
+    }, {
         dishName: 'Salmon',
         ingredients: 'Atlantic Herb Salmon Butter'
-    },{
+    }, {
         dishName: 'Chicken Wings Airfried',
         ingredients: 'Chicken Wings raw Choice of Seasoning(bbq,lemon peppert etc.), Choice of Sauce(bbq, Honey Mustard etc.'
-    },{
+    }, {
         dishName: 'Pork Ribs Airfried',
         ingredients: 'Short Ribs raw, BBQ Seasoning, BBQ Sauce'
     }
@@ -33,16 +33,16 @@ let dishesList = [
 
 let listFromLocal = JSON.parse(localStorage.getItem('dishesList'));
 
-if (listFromLocal !== null){
+if (listFromLocal !== null) {
     dishesList = listFromLocal;
     console.log(dishesList)
 };
 
-// Now and Array of the days of the weeek
+// Array of the days of the weeek
 
 let daysOfWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
 
-//Add event listener for the button to work
+//Add event listener for the button createList to work
 
 document.querySelector('#createList').addEventListener('click', (e) => {
     // Creating a function to get a new Array with 5 random items from Dishes List
@@ -55,38 +55,53 @@ document.querySelector('#createList').addEventListener('click', (e) => {
     tableBody.innerHTML = '';
 
     //foor loop to iterate thru this new 5 dishes Array
-    for (i=0; i < dishesForWeek.length; i++) {
+    for (i = 0; i < dishesForWeek.length; i++) {
         // console.log(daysOfWeek[i], dishesForWeek[i].dishName)
         // now lets update the tables in the HTML Documnet
-        tableBody.innerHTML += `<tr><td>${daysOfWeek[i]}</td><td>${dishesForWeek[i].dishName}</td><td>${dishesForWeek[i].ingredients}</td></tr>`
+        tableBody.innerHTML += `<tr scope="row"><td>${daysOfWeek[i]}</td><td>${dishesForWeek[i].dishName}</td><td>${dishesForWeek[i].ingredients}</td></tr>`
     }
 })
+//slecting proper table body to work on. The full list table
+let fullTableBody = document.querySelector('#fullTableBody');
+// Creating Event to show Full list of items in Array
+window.addEventListener('load', fullList())
+
+
+function fullList () {
+    // for loop to iterate thru the full list array
+    for (i = 0; i < dishesList.length; i++) {
+        fullTableBody.innerHTML += `<tr scope="row"><td>${dishesList[i].dishName}</td><td>${dishesList[i].ingredients}</td><td><button class="btn btn-outline-danger">Remove</button></td></tr>`
+    }
+
+    console.log(fullTableBody)
+}
+
 // Adding New Object of Dish to the dishesList Array
 
 // Creating the event that will create the new dish by clicking sbumit button 
 
 document.querySelector('#addingToList').addEventListener('click', (e) => {
-   // Creating 2 vars to hold the 2 text areas values
-let addDish = document.querySelector('#addDish').value;
-document.querySelector('#addDish').value = '';
-let addIngredients = document.querySelector('#addIngredients').value;
-document.querySelector('#addIngredients').value = '';
+    // Creating 2 vars to hold the 2 text areas values
+    let addDish = document.querySelector('#addDish').value;
+    document.querySelector('#addDish').value = '';
+    let addIngredients = document.querySelector('#addIngredients').value;
+    document.querySelector('#addIngredients').value = '';
 
-if (addDish !== '' && addIngredients !== '' && addDish !== ' ' && addIngredients !== ' '){
-    //creating new Ob with new values
+    if (addDish !== '' && addIngredients !== '' && addDish !== ' ' && addIngredients !== ' ') {
+        //creating new Ob with new values
 
-    let newDish = {
-         dishName: addDish,
-        ingredients: addIngredients
+        let newDish = {
+            dishName: addDish,
+            ingredients: addIngredients
         }
 
-    //adding newDish Ob to Aray DishesList
+        //adding newDish Ob to Aray DishesList
 
-    dishesList.push(newDish);
+        dishesList.push(newDish);
 
-    //Making The updated Array Persisitn by adding it to local Storage.
+        //Making The updated Array Persisitn by adding it to local Storage.
 
-    localStorage.setItem('dishesList', JSON.stringify(dishesList));
+        localStorage.setItem('dishesList', JSON.stringify(dishesList));
     }
 })
 
