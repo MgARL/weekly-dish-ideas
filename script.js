f52c51677526d8b763ae4e247acce56fe6db19d5
Miguel(MgARL) <mgarlcr@gmail.com>
2021-11-07 11:13:50 -0500

    Removed table head from page, and will be generated by Clicking the button, also cahnge buttons color to blue

--- script.js
+++ script.js
@@ -6,27 +6,27 @@ let dishesList = [
     },
     {
         dishName: 'Chicken & Cheese-Broccoli',
-        ingredients: 'Chicken Tenders, Broccolli, Heavy Cream, Bacon, Shreded Mozzarella Cheese'
+        ingredients: 'Chicken Tenders, Broccoli, Heavy Cream, Bacon, Shredded Mozzarella Cheese'
     },
     {
-        dishName: 'Salad With Choice of Meat(Chiken/Beef',
+        dishName: 'Salad With Choice of Meat(Chicken/Beef',
         ingredients: ' Lettuce Mix, Canned Corn, Dressing(Ranch/Alfredo), Choice of Meat(Chicken/Beef)'
     },
     {
-        dishName: 'Steak & Greenbeans/Asparragus',
-        ingredients: 'Steak, Green-bean/Asparragus, Butter, Coice of Sauce(bbq, steak etc.)'
+        dishName: 'Steak & Green-beans/Asparagus',
+        ingredients: 'Steak, Green-bean/Asparagus, Butter, Choice of Sauce(bbq, steak etc.)'
     },
     {
         dishName: 'Meatballs with Cauliflower Rice',
-        ingredients: 'Ground Beef, Your Choice of Sesoning, Riced Cauliflower, Almond flour, Eggs'
+        ingredients: 'Ground Beef, Your Choice of Seasoning, Riced Cauliflower, Almond flour, Eggs'
     }, {
         dishName: 'Salmon',
         ingredients: 'Atlantic Herb Salmon Butter'
     }, {
-        dishName: 'Chicken Wings Airfried',
-        ingredients: 'Chicken Wings raw Choice of Seasoning(bbq,lemon peppert etc.), Choice of Sauce(bbq, Honey Mustard etc.'
+        dishName: 'Chicken Wings Air-fried',
+        ingredients: 'Chicken Wings raw Choice of Seasoning(bbq,lemon pepper etc.), Choice of Sauce(bbq, Honey Mustard etc.'
     }, {
-        dishName: 'Pork Ribs Airfried',
+        dishName: 'Pork Ribs Air-fried',
         ingredients: 'Short Ribs raw, BBQ Seasoning, BBQ Sauce'
     }
 ]
@@ -38,7 +38,7 @@ if (listFromLocal !== null) {
     console.log('List Loaded from local Storage into memory')
 };
 
-// Array of the days of the weeek
+// Array of the days of the week
 
 let daysOfWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
 
@@ -50,22 +50,25 @@ document.querySelector('#createList').addEventListener('click', (e) => {
     let dishesForWeek = shuffledDishesList.slice(0, 5);
 
     // Selecting the table body for later use when updating the dom
-    let tableBody = document.querySelector('#tableBody');
-    // if there is something already in tablebody lets clear it
-    tableBody.innerHTML = '';
+    let table = document.querySelector('#table');
+    // if there is something already in table-body lets clear it
+    table.innerHTML = '<thead class="text-center"><tr><th scope="col-sm-4">Day of the week</th><th scope="col-sm-4">Dish Name</th><th scope="col-sm-4">Ingredients</th></tr></thead>';
+    let tableBody = document.createElement('tbody');
 
-    //foor loop to iterate thru this new 5 dishes Array
+    //for loop to iterate thru this new 5 dishes Array
     for (i = 0; i < dishesForWeek.length; i++) {
         // console.log(daysOfWeek[i], dishesForWeek[i].dishName)
-        // now lets update the tables in the HTML Documnet
+        // now lets update the tables in the HTML Document
         tableBody.innerHTML += `<tr scope="row"><td>${daysOfWeek[i]}</td><td>${dishesForWeek[i].dishName}</td><td>${dishesForWeek[i].ingredients}</td></tr>`
     }
+    table.appendChild(tableBody);
+
     console.log('List for the Week created!')
 })
-//slecting proper table body to work on. The full list table
+//selecting proper table body to work on. The full list table
 let fullTableBody = document.querySelector('#fullTableBody');
 
-//Function to create list in Accordeon
+//Function to create list in Accordion
 function fullList () {
     fullTableBody.innerHTML = '';
     // for loop to iterate thru the full list array
@@ -115,7 +118,7 @@ for( let i = 0; i < allRemoveBtns.length; i++){
 
 // Adding New Object of Dish to the dishesList Array
 
-// Creating the event that will create the new dish by clicking sbumit button 
+// Creating the event that will create the new dish by clicking submit button 
 
 document.querySelector('#addingToList').addEventListener('click', (e) => {
     // Creating 2 vars to hold the 2 text areas values
@@ -136,10 +139,10 @@ document.querySelector('#addingToList').addEventListener('click', (e) => {
 
         dishesList.push(newDish);
         
-        //refreshing the full list in accordeon, so new item shows
+        //refreshing the full list in accordion, so new item shows
         fullList();
 
-        // callling save to storage function
+        // calling save to storage function
         saveToLocal();
     }
 })
